1b804ab26e81d4b2dabcef87b254212eb1e14501
Sagun Raj Lage <sagsraj@yahoo.com>
2018-01-13 17:30:27 +0545

    Twelvth Commit - Added Mailtrap in env, auth middleware basics, active navbar item selection

--- routes/web.php
+++ routes/web.php
@@ -16,25 +16,31 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+Route::middleware('auth')->group(function(){
+    Route::get('/category', 'CategoryController@index')->name('category.index');
+    Route::get('/category/create', 'CategoryController@create')->name('category.create');
+    Route::delete('/category/{id}', 'CategoryController@destroy')->name('category.delete');
+    Route::get('category/{id}/edit', 'CategoryController@edit')->name('category.edit');
+    Route::put('category/{id}', 'CategoryController@update')->name('category.update');
+    Route::post('/category', 'CategoryController@store')->name('category.store');
 
+    Route::get('/category/{id}', 'CategoryController@show')->name('category.show');
+    /*Route::get('/category/{id}/{name}', 'CategoryController@updateData');*/
+    Route::get('/category/{name}/{names}', 'CategoryController@updateName')->name('category.updatename');
+
+    Route::get('/posts', 'PostController@index')->name('post.index');
+    Route::get('/posts/create', 'PostController@create')->name('post.create');
+    Route::get('/posts/{id}/edit', 'PostController@edit')->name('post.edit');
+    Route::get('/posts/{id}', 'PostController@show')->name('post.show');
+    Route::delete('/posts/{id}', 'PostController@destroy')->name('post.delete');
+    Route::put('/posts/{id}', 'PostController@update')->name('post.update');
+    Route::post('/posts', 'PostController@store')->name('post.store');
+
+});
 /*Route::get('/home', 'HomeController@index')->name('home');*/
 
-Route::get('/category', 'CategoryController@index')->name('category.index');
-Route::get('/category/create', 'CategoryController@create')->name('category.create');
-Route::delete('/category/{id}', 'CategoryController@destroy')->name('category.delete');
-Route::get('category/{id}/edit', 'CategoryController@edit')->name('category.edit');
-Route::put('category/{id}', 'CategoryController@update')->name('category.update');
-Route::post('/category', 'CategoryController@store')->name('category.store');
 
-Route::get('/category/{id}', 'CategoryController@show')->name('category.show');
-/*Route::get('/category/{id}/{name}', 'CategoryController@updateData');*/
-Route::get('/category/{name}/{names}', 'CategoryController@updateName')->name('category.updatename');
 
-Route::get('/posts', 'PostController@index')->name('post.index');
-Route::get('/posts/create', 'PostController@create')->name('post.create');
-Route::get('/posts/{id}/edit', 'PostController@edit')->name('post.edit');
-Route::get('/posts/{id}', 'PostController@show')->name('post.show');
-Route::delete('/posts/{id}', 'PostController@destroy')->name('post.delete');
-Route::put('/posts/{id}', 'PostController@update')->name('post.update');
-Route::post('/posts', 'PostController@store')->name('post.store');
+Auth::routes();
 
+Route::get('/home', 'HomeController@index')->name('home');
